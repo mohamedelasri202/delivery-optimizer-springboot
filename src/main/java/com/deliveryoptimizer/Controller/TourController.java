ebc8244850e6cb578e6a533b45fc007ee1f7ff5b
mohamedelasri202 <el7994995@gmail.com>
2025-10-30 17:25:02 +0100

    adding the methode for the total distance and also the comparing of the tow types of algorithms

--- src/main/java/com/deliveryoptimizer/Controller/TourController.java
+++ src/main/java/com/deliveryoptimizer/Controller/TourController.java
@@ -1,6 +1,9 @@
 package com.deliveryoptimizer.Controller;
 
+import com.deliveryoptimizer.DTO.OptimizedTourDTO;
 import com.deliveryoptimizer.DTO.TourDTO;
+import com.deliveryoptimizer.Model.Tour;
+import com.deliveryoptimizer.Service.TourService;
 import com.deliveryoptimizer.Service.TourServiceInterface;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,22 +31,39 @@ public class TourController {
         return ResponseEntity.ok(tourService.update(dto));
     }
 
-//    @GetMapping
-//    public ResponseEntity<List<TourDTO>> getAll() {
-//        return ResponseEntity.ok(tourService.getAll());
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<TourDTO> getById(@PathVariable Integer id) {
-//        TourDTO dto = tourService.getById(id);
-//        if (dto != null) return ResponseEntity.ok(dto);
-//        return ResponseEntity.notFound().build();
-//    }
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> delete(@PathVariable Integer id) {
         Boolean deleted = tourService.delete(id);
         if (deleted) return ResponseEntity.ok("Tour with id " + id + " deleted successfully");
         return ResponseEntity.status(404).body("Tour not found");
+    }
+
+
+    @PostMapping("/{id}/optimize")
+    public ResponseEntity<OptimizedTourDTO> optimize(@PathVariable Integer id) {
+
+        OptimizedTourDTO optimizedTour = tourService.getOptimizedTour(id);
+        if(optimizedTour != null){
+            return ResponseEntity.ok(optimizedTour);
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{id}/compareAlgorithems")
+
+    public ResponseEntity<List<OptimizedTourDTO>> compareAlgorithems(@PathVariable Integer id) {
+
+        List<OptimizedTourDTO> comparisonResults = tourService.compareAlgorithem(id);
+
+        if (comparisonResults != null && !comparisonResults.isEmpty()) {
+
+            return ResponseEntity.ok(comparisonResults);
+        } else {
+
+            return ResponseEntity.notFound().build();
+        }
     }
 }
